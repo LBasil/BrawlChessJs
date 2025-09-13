bfd3a39f71c5c5d765535609c7c3cff5c0e7ae43
Basil L'HÔTE <basil.lhote@gmail.com>
2025-09-13 20:53:04 +0200

    Pawns communications

--- server/index.js
+++ server/index.js
@@ -1,13 +1,32 @@
 import express from 'express';
 import cors from 'cors';
+import fs from 'fs';
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
+// Charge les pions depuis le fichier JSON
+const pawns = JSON.parse(fs.readFileSync('./pawns.json', 'utf-8'));
+
+function generatePlayerPawns(player) {
+  return pawns.map(p => ({
+    ...p,
+    id: `${player}_${p.id}`,
+    owner: player
+  }));
+}
+
 app.post('/start', (req, res) => {
-  console.log('Received start request with body:', req.body);
-  res.json({ status: 'ok' });
+  const player1Pawns = generatePlayerPawns('player1');
+  const player2Pawns = generatePlayerPawns('player2');
+  console.log('Game started with pawns:', { player1Pawns, player2Pawns });
+
+  res.json({
+    status: 'ok',
+    player1Pawns,
+    player2Pawns
+  });
 });
 
 app.listen(3000, () => {
